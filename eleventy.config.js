452aa2cc8bf1e0526f6e2bbd23ec786fc0b17345
Shaun Chong <data-miner00@users.noreply.github.com>
2026-04-19 11:30:48 +0800

    chore: Setup prettier formatter

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,11 @@
 module.exports = function (eleventyConfig) {
   return {
+    templateFormats: ["md", "njk", "html", "liquid", "11ty.js"],
+
     dir: {
       input: "content",
       data: "../_data",
       includes: "../_includes",
-      // layouts: "../_layouts",
     },
   };
 };
